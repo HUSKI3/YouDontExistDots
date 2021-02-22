ca8d06f9227039473b4f5742fa3c665f472586df
Linkus <Check Discord>
2021-02-22 12:27:53 +0100

    Added polybar

---
+++ config/polybar/scripts/spotify_status.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+playerctl metadata --format 'Now playing: {{ artist }} - {{ album }} - {{ title }}'
+#pid = $(pgrep -f "polybar metadata")
+#polybar-msg -p "$pid"
+
+#
+#if [ "$(playerctl --player=playerctld status)" = "Stopped" ]; then
+#    echo "No music is playing"
+#elif [ "$(playerctl --player=playerctld status)" = "Paused"  ]; then
+#    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 2 1>/dev/null 2>&1
+#    playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
+#else # Can be configured to output differently when player is paused
+#    polybar-msg -p "$(pgrep -f "polybar now-playing")" hook spotify-play-pause 1 1>/dev/null 2>&1
+#    playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
+#fi
